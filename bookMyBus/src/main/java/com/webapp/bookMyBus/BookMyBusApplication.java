4a98b1f142142c3ee114731bf6b40d903c1f1245
WhackyFool <harshitvidhyarthi@gmail.com>
2019-12-05 15:45:03 +0530

    initializing repo

---
+++ bookMyBus/src/main/java/com/webapp/bookMyBus/BookMyBusApplication.java
@@ -0,0 +1,11 @@
+package com.webapp.bookMyBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyBusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyBusApplication.class, args);
+	}
+}
---
+++ bookMyBus/src/test/java/com/webapp/bookMyBus/BookMyBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.bookMyBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
